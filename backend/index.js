c10acdca0b42a73d386fc295a7b0052dc141e7db
reshmakuchekar <132090872+reshmakuchekar@users.noreply.github.com>
2024-12-30 09:55:14 +0530

    Initial Commit of task

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors=require('cors');
+const userroute = require('./src/routes/routes');
+
+const PORT = 1234;
+
+const url = 'mongodb+srv://reshmakuchekar16:LLLcqoCCu2YhosXj@cluster0.brmjb.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
+
+app.use(cors({
+  origin:'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+}))
+app.use('/', userroute);
+
+app.listen(PORT, () => {
+    console.log(`Server Started listening on PORT ${PORT}`);
+});
